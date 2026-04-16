10e124629a2f4e7451f0eb4409bbb08975be0c51
Do Ky Dung <154410363+ZuuSaitoh@users.noreply.github.com>
2026-04-16 08:31:29 +0700

    Revert "update path"
    This reverts commit 78c822c8a03b6b61a77e660dc272c32df88e8002.

--- app/(screens)/provider-order-detail.tsx
+++ app/(screens)/provider-order-detail.tsx
@@ -15,7 +15,7 @@ import {
   View,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import axiosClient from "../../api/axiosClient";
+import axiosClient from "../api/axiosClient";
 
 const { width: screenWidth } = Dimensions.get("window");
 
