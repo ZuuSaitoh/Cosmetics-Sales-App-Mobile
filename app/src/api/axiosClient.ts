90960e87d2460eda458fa67c231181684656e2e3
Do Ky Dung <dokydungtnt1072004@gmail.com>
2026-04-22 23:14:41 +0700

    Update axiosClient.ts

--- app/src/api/axiosClient.ts
+++ app/src/api/axiosClient.ts
@@ -1,8 +1,8 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios, { InternalAxiosRequestConfig } from "axios";
 
-export const API_BASE_URL = "http://171.232.184.122:8080/api";// export const API_BASE_URL = "http://
-export const WS_BASE_URL = "ws://171.232.184.122:8080/ws-mobile";
+export const API_BASE_URL = "http://115.77.242.89:8080/api"; // export const API_BASE_URL = "http://
+export const WS_BASE_URL = "ws://115.77.242.89:8080/ws-mobile";
 
 const axiosClient = axios.create({
   baseURL: API_BASE_URL,
